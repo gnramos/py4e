4582c3ad04b1d3c339a3df9b9364a935490acf97
gnramos <gnramos@users.noreply.github.com>
2021-10-21 16:29:36 -0300

    Update fahren.py
    Renomeando variável para condizer com o livro texto.

--- code3/fahren.py
+++ code3/fahren.py
@@ -1,4 +1,4 @@
-inp = input('Insira a temperatura em Fahrenheit: ')
-fahr = float(inp)
+ent = input('Insira a temperatura em Fahrenheit: ')
+fahr = float(ent)
 cel = (fahr - 32.0) * 5.0 / 9.0
 print(cel)
